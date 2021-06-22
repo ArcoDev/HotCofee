ebbf5991a55ebc26cf2096627bee9c5b887512a0
arcodev <chris_arco0794@hotmail.com>
2021-06-22 16:05:21 -0500

    Trayendo la informacion de los articles desde la BD

--- assets/templates/content.php
+++ assets/templates/content.php
@@ -1,80 +1,31 @@
 <main class="container">
     <article class="grid-container">
-        <div class="article">
-            <div class="info-article">
-                <h2>long established</h2>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page
-                    when looking at its layout. The point of using Lorem Ipsum is that....</p>
-                <div class="footer-info">
-                    <p>May 20th 2020</p>
-                    <a href="<?php echo get_template_directory_uri(); ?>/article.php">Read more</a>
-                </div>
-            </div>
-            <div class="img-article">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image1.png" alt="Image HotCoffee">
-            </div>
-        </div>
-        <div class="article">
-            <div class="img-article">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image2.png" alt="Image HotCoffee">
-            </div>
-            <div class="info-article">
-                <h2>long established</h2>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page
-                    when looking at its layout. The point of using Lorem Ipsum is that....</p>
-                <div class="footer-info">
-                    <p>May 20th 2020</p>
-                    <a href="<?php echo get_template_directory_uri(); ?>/article.php">Read more</a>
-                </div>
-            </div>
-        </div>
-        <div class="article">
-            <div class="img-article">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image3.png" alt="Image HotCoffee">
-            </div>
-            <div class="info-article">
-                <h2>long established</h2>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page
-                    when looking at its layout. The point of using Lorem Ipsum is that....</p>
-                <div class="footer-info">
-                    <p>May 20th 2020</p>
-                    <a href="<?php echo get_template_directory_uri(); ?>/article.php">Read more</a>
-                </div>
-            </div>
-        </div>
-        <div class="article">
-            <div class="img-article">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image4.png" alt="Image HotCoffee">
-            </div>
-            <div class="info-article">
-                <h2>long established</h2>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page
-                    when looking at its layout. The point of using Lorem Ipsum is that....</p>
-                <div class="footer-info">
-                    <p>May 20th 2020</p>
-                    <a href="<?php echo get_template_directory_uri(); ?>/article.php">Read more</a>
-                </div>
-            </div>
-        </div>
-        <div class="article">
-            <div class="info-article">
-                <h2>What is Lorem Ipsum?</h2>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page
-                    when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
-                    distribution...</p>
-                <div class="footer-info">
-                    <p>May 20</p>
-                    <a href="<?php echo get_template_directory_uri(); ?>/article.php">Read more</a>
-                </div>
-            </div>
-            <div class="img-article">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image5.png" alt="Image HotCoffee">
-            </div>
-        </div>
+        <?php   
+            $route = get_template_directory_uri();
+            $sql =  $con->query("SELECT * FROM article");
+            while ($articles = mysqli_fetch_array($sql)) {
+                echo '<div id="'.$articles['id_article'].'" class="article">
+                            <div class="info-article">
+                                <h2>'.$articles['title'].'</h2>
+                                <p>'.$articles['description'].'</p>
+                                <div class="footer-info">
+                                    <p>'.$articles['date'].'</p>
+                                    <a href="'.$route.$articles['enlace'].'">Read more</a>
+                                </div>
+                            </div>
+                            <div class="img-article">
+                                <img src="'.$route.$articles['image'].'" alt="Image HotCoffee">
+                            </div>
+                        </div>';
+            }
+        ?>
     </article>
+    <!-- Button Next Page -->
     <aside class="btn-aside">
-        <a href="#" class="btn-next">Next Page <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/next.svg" alt="Next Icon"></a>
+        <a href="#" class="btn-next">Next Page <img
+                src="<?php echo get_template_directory_uri(); ?>/assets/icons/next.svg" alt="Next Icon"></a>
     </aside>
+    <!-- Contact Form -->
     <section class="contact">
         <div class="title">
             <h2>Contact</h2>
